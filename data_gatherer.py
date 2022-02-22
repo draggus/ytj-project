0f588650193fa6fc6dede85fba2d556c8c4d5d0f
draggus <97789141+draggus@users.noreply.github.com>
2022-02-23 01:05:45 +0200

    Add files via upload
    deadline approaching so switching to work on the front side of it

--- data_gatherer.py
+++ data_gatherer.py
@@ -1,12 +1,46 @@
 import requests
 import json
-response = requests.get('https://avoindata.prh.fi/bis/v1/2532004-3')
+bisid = str(input("Enter a businessID: "))
+response = requests.get('https://avoindata.prh.fi/bis/v1/'+ (bisid))
 print(response.status_code)
 company = response.json()
 print ("Business ID: ", company['results'][0]['businessId'])
 print ("Company Name: ", company['results'][0]['name'])
-print ("Address: ", company['results'][0]['addresses'][1]['street'])
-print ("Post Code: ", company['results'][0]['addresses'][1]['postCode'])
-print ("City: ", company['results'][0]['addresses'][1]['city'])
-print ("Phone Number: ", company['results'][0]['contactDetails'][3]['value'])
-print ("Webpage: ", company['results'][0]['contactDetails'][9]['value'])
+for x in company['results'][0]['addresses']:
+    if x['street'] == "":
+        continue
+    elif x['street'] == None:
+        continue
+    else:
+        print("Address: ", x['street'])
+        break
+for x in company['results'][0]['addresses']:
+    if x['postCode'] == "":
+        continue
+    elif x['postCode'] == None :
+        continue
+    else:
+        print("Post Code: ", x['postCode'])
+        break
+for x in company['results'][0]['addresses']:
+    if x['city'] == "":
+        continue
+    elif x['city'] == None :
+        continue
+    else:
+        print("City: ", x['city'])
+        break
+for x in company['results'][0]['contactDetails']:
+    if ["Puhelin"]:
+        if x['value'] == "":
+            continue
+        elif x['value'] == None:
+            continue
+        else:
+            print("Phone Number: ",x['value'])
+            break
+    else:
+        continue
+#This part I just couldn't get to work for now..
+print("Webpage: ", company['results'][0]['contactDetails'][9]['value'])
+#This part I just couldn't get to work for now..
